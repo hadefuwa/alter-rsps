67fd4820e023280100bd7628def8aaec401fe67d
Hamed Adefuwa <hamed.adefuwa@gmail.com>
2025-11-27 21:25:32 +0000

    Halve NPC spawns in God Wars Dungeon

--- game-plugins/src/main/kotlin/org/alter/plugins/content/areas/godwars/spawns/SpawnPlugin.kt
+++ game-plugins/src/main/kotlin/org/alter/plugins/content/areas/godwars/spawns/SpawnPlugin.kt
@@ -20,21 +20,14 @@ class SpawnPlugin(
         knight_of_saradomin:2213
         knight_of_saradomin_2214:2214
         */
-        // Saradomin Spawns
+        // Saradomin Spawns (halved)
         spawnNpc(npc = "npc.spiritual_warrior", x = 2909, z = 5299, height = 2, walkRadius = 5, direction = Direction.SOUTH)
-        spawnNpc(npc = "npc.spiritual_warrior", x = 2897, z = 5303, height = 2, walkRadius = 5, direction = Direction.SOUTH)
-        spawnNpc(npc = "npc.spiritual_warrior", x = 2891, z = 5299, height = 2, walkRadius = 5, direction = Direction.SOUTH)
         
         spawnNpc(npc = "npc.spiritual_mage", x = 2906, z = 5301, height = 2, walkRadius = 5, direction = Direction.SOUTH)
-        spawnNpc(npc = "npc.spiritual_mage", x = 2894, z = 5303, height = 2, walkRadius = 5, direction = Direction.SOUTH)
-        spawnNpc(npc = "npc.spiritual_mage", x = 2890, z = 5297, height = 2, walkRadius = 5, direction = Direction.SOUTH)
 
         spawnNpc(npc = "npc.spiritual_ranger", x = 2902, z = 5302, height = 2, walkRadius = 5, direction = Direction.SOUTH)
-        spawnNpc(npc = "npc.spiritual_ranger", x = 2891, z = 5303, height = 2, walkRadius = 5, direction = Direction.SOUTH)
-        spawnNpc(npc = "npc.spiritual_ranger", x = 2888, z = 5295, height = 2, walkRadius = 5, direction = Direction.SOUTH)
 
         spawnNpc(npc = "npc.knight_of_saradomin", x = 2901, z = 5303, height = 2, walkRadius = 5, direction = Direction.SOUTH)
-        spawnNpc(npc = "npc.knight_of_saradomin_2214", x = 2889, z = 5304, height = 2, walkRadius = 5, direction = Direction.SOUTH)
 
 
         /* NPC IDs
@@ -44,25 +37,16 @@ class SpawnPlugin(
         goblin_2245:2245
         goblin_2246:2246
         */
-        // Bandos Spawns
+        // Bandos Spawns (halved)
         spawnNpc(npc = "npc.spiritual_warrior_2243", x = 2854, z = 5333, height = 2, walkRadius = 5, direction = Direction.SOUTH)
-        spawnNpc(npc = "npc.spiritual_warrior_2243", x = 2857, z = 5327, height = 2, walkRadius = 5, direction = Direction.SOUTH)
-        spawnNpc(npc = "npc.spiritual_warrior_2243", x = 2861, z = 5325, height = 2, walkRadius = 5, direction = Direction.SOUTH)
         
         spawnNpc(npc = "npc.spiritual_mage_2244", x = 2856, z = 5331, height = 2, walkRadius = 5, direction = Direction.SOUTH)
-        spawnNpc(npc = "npc.spiritual_mage_2244", x = 2859, z = 5325, height = 2, walkRadius = 5, direction = Direction.SOUTH)
-        spawnNpc(npc = "npc.spiritual_mage_2244", x = 2858, z = 5321, height = 2, walkRadius = 5, direction = Direction.SOUTH)
 
         spawnNpc(npc = "npc.spiritual_ranger_2242", x = 2857, z = 5329, height = 2, walkRadius = 5, direction = Direction.SOUTH)
-        spawnNpc(npc = "npc.spiritual_ranger_2242", x = 2861, z = 5325, height = 2, walkRadius = 5, direction = Direction.SOUTH)
-        spawnNpc(npc = "npc.spiritual_ranger_2242", x = 2857, z = 5322, height = 2, walkRadius = 5, direction = Direction.SOUTH)
 
         spawnNpc(npc = "npc.goblin_2245", x = 2901, z = 5303, height = 2, walkRadius = 5, direction = Direction.SOUTH)
         spawnNpc(npc = "npc.goblin_2246", x = 2889, z = 5304, height = 2, walkRadius = 5, direction = Direction.SOUTH)
         spawnNpc(npc = "npc.goblin_2245", x = 2886, z = 5327, height = 2, walkRadius = 5, direction = Direction.SOUTH)
-        spawnNpc(npc = "npc.goblin_2246", x = 2883, z = 5325, height = 2, walkRadius = 5, direction = Direction.SOUTH)
-        spawnNpc(npc = "npc.goblin_2245", x = 2880, z = 5323, height = 2, walkRadius = 5, direction = Direction.SOUTH)
-        spawnNpc(npc = "npc.goblin_2246", x = 2877, z = 5321, height = 2, walkRadius = 5, direction = Direction.SOUTH)
 
 
 
@@ -72,26 +56,17 @@ class SpawnPlugin(
         spiritual_mage_3168:3168
         aviansie:3169 (use "npc.aviansie" not "npc.aviansie_3169")
         */
-        // Armadyl Spawns
+        // Armadyl Spawns (halved)
 
         spawnNpc(npc = "npc.spiritual_warrior_3166", x = 2871, z = 5281, height = 2, walkRadius = 10, direction = Direction.SOUTH)
-        spawnNpc(npc = "npc.spiritual_warrior_3166", x = 2874, z = 5287, height = 2, walkRadius = 10, direction = Direction.SOUTH)
-        spawnNpc(npc = "npc.spiritual_warrior_3166", x = 2873, z = 5284, height = 2, walkRadius = 10, direction = Direction.SOUTH)
         
         spawnNpc(npc = "npc.spiritual_mage_3168", x = 2869, z = 5284, height = 2, walkRadius = 10, direction = Direction.SOUTH)
-        spawnNpc(npc = "npc.spiritual_mage_3168", x = 2877, z = 5284, height = 2, walkRadius = 10, direction = Direction.SOUTH)
-        spawnNpc(npc = "npc.spiritual_mage_3168", x = 2876, z = 5281, height = 2, walkRadius = 10, direction = Direction.SOUTH)
 
         spawnNpc(npc = "npc.spiritual_ranger_3167", x = 2871, z = 5287, height = 2, walkRadius = 10, direction = Direction.SOUTH)
-        spawnNpc(npc = "npc.spiritual_ranger_3167", x = 2875, z = 5282, height = 2, walkRadius = 10, direction = Direction.SOUTH)
-        spawnNpc(npc = "npc.spiritual_ranger_3167", x = 2879, z = 5287, height = 2, walkRadius = 10, direction = Direction.SOUTH)
 
         spawnNpc(npc = "npc.aviansie", x = 2870, z = 5284, height = 2, walkRadius = 5, direction = Direction.SOUTH)
         spawnNpc(npc = "npc.aviansie", x = 2868, z = 5289, height = 2, walkRadius = 5, direction = Direction.SOUTH)
         spawnNpc(npc = "npc.aviansie", x = 2863, z = 5289, height = 2, walkRadius = 5, direction = Direction.SOUTH)
-        spawnNpc(npc = "npc.aviansie", x = 2861, z = 5294, height = 2, walkRadius = 5, direction = Direction.SOUTH)
-        spawnNpc(npc = "npc.aviansie", x = 2877, z = 5286, height = 2, walkRadius = 5, direction = Direction.SOUTH)
-        spawnNpc(npc = "npc.aviansie", x = 2876, z = 5282, height = 2, walkRadius = 5, direction = Direction.SOUTH)
 
 
          /* NPC IDs
@@ -100,26 +75,17 @@ class SpawnPlugin(
         spiritual_mage_3161:3161
         imp:3134
         */
-        // Zamarok Spawns
+        // Zamarok Spawns (halved)
 
         spawnNpc(npc = "npc.spiritual_warrior_3159", x = 2885, z = 5328, height = 2, walkRadius = 10, direction = Direction.SOUTH)
-        spawnNpc(npc = "npc.spiritual_warrior_3159", x = 2891, z = 5326, height = 2, walkRadius = 10, direction = Direction.SOUTH)
-        spawnNpc(npc = "npc.spiritual_warrior_3159", x = 2887, z = 5331, height = 2, walkRadius = 10, direction = Direction.SOUTH)
         
         spawnNpc(npc = "npc.spiritual_mage_3161", x = 2880, z = 5331, height = 2, walkRadius = 10, direction = Direction.SOUTH)
-        spawnNpc(npc = "npc.spiritual_mage_3161", x = 2876, z = 5334, height = 2, walkRadius = 10, direction = Direction.SOUTH)
-        spawnNpc(npc = "npc.spiritual_mage_3161", x = 2877, z = 5327, height = 2, walkRadius = 10, direction = Direction.SOUTH)
 
         spawnNpc(npc = "npc.spiritual_ranger_3160", x = 2872, z = 5329, height = 2, walkRadius = 10, direction = Direction.SOUTH)
-        spawnNpc(npc = "npc.spiritual_ranger_3160", x = 2877, z = 5325, height = 2, walkRadius = 10, direction = Direction.SOUTH)
-        spawnNpc(npc = "npc.spiritual_ranger_3160", x = 2896, z = 5322, height = 2, walkRadius = 10, direction = Direction.SOUTH)
 
         spawnNpc(npc = "npc.imp_3134", x = 2901, z = 5303, height = 2, walkRadius = 5, direction = Direction.SOUTH)
         spawnNpc(npc = "npc.imp_3134", x = 2889, z = 5304, height = 2, walkRadius = 5, direction = Direction.SOUTH)
         spawnNpc(npc = "npc.imp_3134", x = 2886, z = 5327, height = 2, walkRadius = 5, direction = Direction.SOUTH)
-        spawnNpc(npc = "npc.imp_3134", x = 2883, z = 5325, height = 2, walkRadius = 5, direction = Direction.SOUTH)
-        spawnNpc(npc = "npc.imp_3134", x = 2880, z = 5323, height = 2, walkRadius = 5, direction = Direction.SOUTH)
-        spawnNpc(npc = "npc.imp_3134", x = 2877, z = 5321, height = 2, walkRadius = 5, direction = Direction.SOUTH)
 
 
         // Item spawns
